b59a3f14e4bd424eb82522e98307960f18cea894
flor.barbosa <flor.barbosa@axented.com>
2022-04-12 18:51:25 -0500

    Add model tests

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:customer) }
+  it { should have_many(:order_products) }
 end
